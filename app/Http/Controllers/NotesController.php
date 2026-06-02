43ee8775df96c1023e085f8a8096243760075630
fauza-alifa <fauzahralifa@gmail.com>
2026-06-02 08:57:11 +0700

    some fixing of notes

--- app/Http/Controllers/NotesController.php
+++ app/Http/Controllers/NotesController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
-use App\Models\Notes;
+use App\Models\Note;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -12,9 +12,9 @@ class NotesController extends Controller
     public function index()
     {
         $categories = Category::all();
-        $notes = Notes::latest()->get();
+        $notes = Note::with('category')->latest()->get();
 
-        return view('notes.index', compact('categories', 'notes'));
+        return view('notes.index', compact('categories'), compact('notes'));
     }
 
     public function create()
@@ -26,21 +26,43 @@ class NotesController extends Controller
 
     public function store(Request $request)
     {
-        $data = $request->validate([
+        // dd($request->all());
+
+        $request->validate([
             'title' => 'required|string|max:150',
-            'content' => 'nullable|string',
-            'category_id' => 'nullable|exists:categories,id',
-            'bg_color' => 'nullable|string',
+            'content' => 'required|string',
+            'category_id' => 'required|exists:categories,id',
+            'bg_color' => 'nullable|string' 
         ]);
 
-        $note = Notes::create([
-            'title' => $data['title'],
-            'content' => $data['content'] ?? null,
-            'category_id' => $data['category_id'] ?? null,
-            'bg_color' => $data['bg_color'] ?? 'bg-brand-purple',
+        Note::create([
+            'category_id' => $request->category_id,
+            'title' => $request->title,
+            'content' => $request->content,
+            'bg_color' => $request->bg_color ?? 'bg-brand-purple'
         ]);
 
-        return redirect()->route('notes.index')->with('success', 'Note saved.');
+        return redirect(route('notes.index'))->with('success', 'Note added succesfully');
     }
 
+    public function edit($id)
+    {
+        $categories =  Category::all();
+        $note = Note::with('category')->findOrFail($id);
+        return view('notes.edit', compact('categories', 'note'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $note = Note::findOrFail($id);
+        $note->update($request->all());
+        return redirect(route('notes.index'))->with('success', 'Note updated succesfully');
+    }
+
+    public function destroy($id)
+    {
+        $note = Note::findOrFail($id);
+        $note->delete();
+        return redirect(route('notes.index'))->with('success','Note deletes succesfully');
+    }
 }
--- resources/views/notes/create.blade.php
+++ resources/views/notes/create.blade.php
@@ -38,26 +38,26 @@
                 </select>
             </div>
             @error('title')
-                    <p class="px-2 py-1 bg-brand-red text-white text-xs mt-1 rounded-md">{{ $message }}</p>
-                @enderror
+                <p class="px-2 py-1 bg-brand-red text-white text-xs mt-1 rounded-md">{{ $message }}</p>
+            @enderror
             @error('category_id')
-                    <p class="px-2 py-1 bg-brand-red text-white text-xs mt-1 rounded-md">{{ $message }}</p>
-                @enderror
+                <p class="px-2 py-1 bg-brand-red text-white text-xs mt-1 rounded-md">{{ $message }}</p>
+            @enderror
 
             <div class="flex items-center text-white/70 mt-1 ml-4 gap-2 shrink-0 border-b border-white/30 pb-2">
                 <i class="ri-calendar-event-fill text-xl"></i>
                 <span class="ml-2">Created at: {{ now()->format('F j, Y') }}</span>
             </div>
 
-            <div class="flex-1 overflow-y-auto my-4 mx-4">
+            <div class="flex-1 overflow-y-auto my-4">
                 <div id="richTextEditor" contenteditable="true" role="textbox" aria-multiline="true"
                     class="w-full h-full min-h-20 border-none bg-brand-purple text-white focus:outline-none resize-none overflow-y-auto outline-none transition-colors duration-200"
                     placeholder="Write your notes here...">{{ old('content') }}</div>
-                
+
             </div>
             @error('content')
-                    <p class="mb-2 px-2 py-1 bg-brand-red text-white text-xs mt-1 rounded-md">{{ $message }}</p>
-                @enderror
+                <p class="mb-2 px-2 py-1 bg-brand-red text-white text-xs mt-1 rounded-md">{{ $message }}</p>
+            @enderror
 
             <div class="flex items-center justify-between shrink-0">
                 <div class="flex gap-3">
@@ -87,14 +87,6 @@
                 </div>
 
                 <div class="flex items-center gap-4 shrink-0">
-                    <form id="deleteForm" method="POST" class="flex-1">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit"
-                            class="bg-brand-red text-black px-8 py-3 font-semibold rounded-xl hover:bg-red-700 active:scale-98 transition shadow-sm">
-                            Delete Note
-                        </button>
-                    </form>
                     <button type="submit"
                         class="bg-brand-yellow text-black px-8 py-3 font-semibold rounded-xl hover:bg-brand-orange active:scale-98 transition shadow-sm">
                         Save Note
