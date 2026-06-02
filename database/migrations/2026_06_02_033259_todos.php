756c824f224ea93026f3049195fb2ed19f73dafd
fauza-alifa <fauzahralifa@gmail.com>
2026-06-02 11:08:39 +0700

    notes table fixing

--- database/migrations/2026_06_02_033259_todos.php
+++ database/migrations/2026_06_02_033259_todos.php
@@ -4,16 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('todos', function (Blueprint $table) {
+        Schema::create('notes', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->boolean('is_completed')->default(false);
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->string('title', 150);
+            $table->text('content')->nullable();
+            $table->string('bg_color')->default('bg-brand-purple');
             $table->timestamps();
         });
     }
@@ -23,6 +26,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('todos');
+        Schema::dropIfExists('notes');
     }
 };
